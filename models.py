b4ff026578eebe3c6c0a138dc6c9d112ee241037
Srinjoy Bhuiya <srinjoybhuiya@gmail.com>
2021-04-30 19:23:38 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,210 @@
+from __future__ import division
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, UpSampling2D, Reshape, core, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as K
+from keras.utils.vis_utils import plot_model as plot
+from keras.optimizers import SGD
+from keras.optimizers import *
+from keras.layers import *        
+import numpy as np
+import keras.layers as layers
+
+def U_net(input_size = (256,256,1)):
+    N = input_size[0]
+    inputs = Input(input_size) 
+    c0 = layers.Conv2D(64, activation='relu', kernel_size=3)(inputs)
+    c1 = layers.Conv2D(64, activation='relu', kernel_size=3)(c0)  # This layer for concatenating in the expansive part
+    c2 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(c1)
+
+    c3 = layers.Conv2D(128, activation='relu', kernel_size=3)(c2)
+    c4 = layers.Conv2D(128, activation='relu', kernel_size=3)(c3)  # This layer for concatenating in the expansive part
+    c5 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(c4)
+
+    c6 = layers.Conv2D(256, activation='relu', kernel_size=3)(c5)
+    c7 = layers.Conv2D(256, activation='relu', kernel_size=3)(c6)  # This layer for concatenating in the expansive part
+    c8 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(c7)
+
+    c9 = layers.Conv2D(512, activation='relu', kernel_size=3)(c8)
+    c10 = layers.Conv2D(512, activation='relu', kernel_size=3)(c9)  # This layer for concatenating in the expansive part
+    c11 = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(c10)
+
+    c12 = layers.Conv2D(1024, activation='relu', kernel_size=3)(c11)
+    c13 = layers.Conv2D(1024, activation='relu', kernel_size=3, padding='valid')(c12)
+
+    # We will now start the second part of the U - expansive part
+    t01 = layers.Conv2DTranspose(512, kernel_size=2, strides=(2, 2), activation='relu')(c13)
+    crop01 = layers.Cropping2D(cropping=(4, 4))(c10)
+
+    concat01 = layers.concatenate([t01, crop01], axis=-1)
+
+    c14 = layers.Conv2D(512, activation='relu', kernel_size=3)(concat01)
+    c15 = layers.Conv2D(512, activation='relu', kernel_size=3)(c14)
+
+    t02 = layers.Conv2DTranspose(256, kernel_size=2, strides=(2, 2), activation='relu')(c15)
+    crop02 = layers.Cropping2D(cropping=(16, 16))(c7)
+
+    concat02 = layers.concatenate([t02, crop02], axis=-1)
+
+    c16 = layers.Conv2D(256, activation='relu', kernel_size=3)(concat02)
+    c17 = layers.Conv2D(256, activation='relu', kernel_size=3)(c16)
+
+    t03 = layers.Conv2DTranspose(128, kernel_size=2, strides=(2, 2), activation='relu')(c17)
+    crop03 = layers.Cropping2D(cropping=(40, 40))(c4)
+
+    concat03 = layers.concatenate([t03, crop03], axis=-1)
+
+    c18 = layers.Conv2D(128, activation='relu', kernel_size=3)(concat03)
+    c19 = layers.Conv2D(128, activation='relu', kernel_size=3)(c18)
+
+    t04 = layers.Conv2DTranspose(64, kernel_size=2, strides=(2, 2), activation='relu')(c19)
+    crop04 = layers.Cropping2D(cropping=(88, 88))(c1)
+
+    concat04 = layers.concatenate([t04, crop04], axis=-1)
+
+    c20 = layers.Conv2D(64, activation='relu', kernel_size=3)(concat04)
+    c21 = layers.Conv2D(64, activation='relu', kernel_size=3)(c20)
+
+    outputs = layers.Conv2D(2, kernel_size=1)(c21)
+
+    model = Model(inputs,outputs)
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
+
+
+def BCDU_net_D3(input_size = (256,256,1)):
+    N = input_size[0]
+    inputs = Input(input_size) 
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+  
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    drop3 = Dropout(0.5)(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # D1
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)     
+    conv4_1 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4_1 = Dropout(0.5)(conv4_1)
+    # D2
+    conv4_2 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(drop4_1)     
+    conv4_2 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_2)
+    conv4_2 = Dropout(0.5)(conv4_2)
+    # D3
+    merge_dense = concatenate([conv4_2,drop4_1], axis = 3)
+    conv4_3 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge_dense)     
+    conv4_3 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4_3)
+    drop4_3 = Dropout(0.5)(conv4_3)
+    up6 = Conv2DTranspose(256, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(drop4_3)
+    up6 = BatchNormalization(axis=3)(up6)
+    up6 = Activation('relu')(up6)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(drop3)
+    x2 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(up6)
+    merge6  = concatenate([x1,x2], axis = 1) 
+    merge6 = ConvLSTM2D(filters = 128, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge6)
+            
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2DTranspose(128, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(conv6)
+    up7 = BatchNormalization(axis=3)(up7)
+    up7 = Activation('relu')(up7)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(conv2)
+    x2 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(up7)
+    merge7  = concatenate([x1,x2], axis = 1) 
+    merge7 = ConvLSTM2D(filters = 64, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge7)
+        
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2DTranspose(64, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(conv7)
+    up8 = BatchNormalization(axis=3)(up8)
+    up8 = Activation('relu')(up8)    
+
+    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
+    x2 = Reshape(target_shape=(1, N, N, 64))(up8)
+    merge8  = concatenate([x1,x2], axis = 1) 
+    merge8 = ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge8)    
+    
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv8 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv9 = Conv2D(1, 1, activation = 'sigmoid')(conv8)
+
+    model = Model(inputs,conv9)
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model
+
+
+
+        
+def BCDU_net_D1(input_size = (256,256,1)):
+    N = input_size[0]
+    inputs = Input(input_size) 
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+  
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    drop3 = Dropout(0.5)(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # D1
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)     
+    conv4_1 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4_1 = Dropout(0.5)(conv4_1)   
+    
+    up6 = Conv2DTranspose(256, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(drop4_1)
+    up6 = BatchNormalization(axis=3)(up6)
+    up6 = Activation('relu')(up6)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(drop3)
+    x2 = Reshape(target_shape=(1, np.int32(N/4), np.int32(N/4), 256))(up6)
+    merge6  = concatenate([x1,x2], axis = 1) 
+    merge6 = ConvLSTM2D(filters = 128, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge6)
+            
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2DTranspose(128, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(conv6)
+    up7 = BatchNormalization(axis=3)(up7)
+    up7 = Activation('relu')(up7)
+
+    x1 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(conv2)
+    x2 = Reshape(target_shape=(1, np.int32(N/2), np.int32(N/2), 128))(up7)
+    merge7  = concatenate([x1,x2], axis = 1) 
+    merge7 = ConvLSTM2D(filters = 64, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge7)
+        
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2DTranspose(64, kernel_size=2, strides=2, padding='same',kernel_initializer = 'he_normal')(conv7)
+    up8 = BatchNormalization(axis=3)(up8)
+    up8 = Activation('relu')(up8)    
+
+    x1 = Reshape(target_shape=(1, N, N, 64))(conv1)
+    x2 = Reshape(target_shape=(1, N, N, 64))(up8)
+    merge8  = concatenate([x1,x2], axis = 1) 
+    merge8 = ConvLSTM2D(filters = 32, kernel_size=(3, 3), padding='same', return_sequences = False, go_backwards = True,kernel_initializer = 'he_normal' )(merge8)    
+    
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv8 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+    conv9 = Conv2D(1, 1, activation = 'sigmoid')(conv8)
+
+    model = Model(inputs,conv9)
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return model   
+    
+    
+    
